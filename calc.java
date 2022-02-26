b715b22396060f9fe34de14a0f359bf0aa6fe849
Sunitha <sunics@gmail.com>
2022-02-26 10:26:12 +0530

    hello

---
+++ calc.java
@@ -0,0 +1,12 @@
+public class calc
+{
+    public static void main(String s[])
+    {
+        int a,b;
+        a=20;
+        b=45;
+        int c=a+b;
+        System.out.println("Sum of "+a+" and "+b+" is "+c);
+    }
+
+}
